c4bff6973cc63d8c45744fc1fc1134e14cce36e4
Subado <redcatgamer1980@gmail.com>
2022-11-20 23:08:50 +0400

    Suppress error messages from lang servers added

--- lua/plugins/lsp/handlers.lua
+++ lua/plugins/lsp/handlers.lua
@@ -103,4 +103,16 @@ local cmp_nvim_lsp = require('cmp_nvim_lsp')
 
 M.capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 
+-- suppress error messages from lang servers
+--[[ vim.notify = function(msg, log_level, _opts)
+		if msg:match("exit code") then
+				return
+		end
+		if log_level == vim.log.levels.ERROR then
+				vim.api.nvim_err_writeln(msg)
+		else
+				vim.api.nvim_echo({{msg}}, true, {})
+		end
+end ]]
+
 return M
