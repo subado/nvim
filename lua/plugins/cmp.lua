d29a2e0f42e0b2f7c37812c2d750f4602eb33a15
Subado <redcatgamer1980@gmail.com>
2022-12-24 23:59:33 +0400

    Edit nvim-cmp mapping

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -17,7 +17,7 @@ cmp.setup {
 				end,
 	},
 	mapping = {
-		["<Tab>"] = cmp.mapping(function(fallback)
+		["<C-j>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
 			elseif luasnip.expand_or_jumpable() then
@@ -29,7 +29,7 @@ cmp.setup {
 			end
 		end, { "i", "s" }),
 
-		["<S-Tab>"] = cmp.mapping(function(fallback)
+		["<C-k>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
 			elseif luasnip.jumpable(-1) then
@@ -38,8 +38,8 @@ cmp.setup {
 				fallback()
 			end
 		end, { "i", "s" }),
-		["<C-k>"] = cmp.mapping.select_prev_item(),
-		["<C-j>"] = cmp.mapping.select_next_item(),
+		["<Tab>"] = cmp.mapping.select_prev_item(),
+		["<S-Tab>"] = cmp.mapping.select_next_item(),
 		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
 		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
 		["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
