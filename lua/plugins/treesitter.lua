0187259b2ce6fa1718d04d8a9e8dba454db89fef
Subado <redcatgamer1980@gmail.com>
2022-12-29 12:40:12 +0400

    Disable treesitter indent

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,8 @@
 require'nvim-treesitter.configs'.setup {
 	ensure_installed = {"cpp", "python", "lua", "bash"}, -- Only use parsers that are maintained
 
-	highlight = { -- enable highlighting
-  enable = true,
-	},
-
-	indent = {
-	  enable = true, -- default is disabled anyways
+	highlight = {
+		enable = true
 	}
+
 }
