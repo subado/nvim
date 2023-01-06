1e4d97d6af7776c689b07d11bfa8bdbbbc4c3705
Subado <redcatgamer1980@gmail.com>
2023-01-06 14:18:43 +0400

    Replace cmake with neocmake

--- lua/plugins/lsp/mason-lspconfig.lua
+++ lua/plugins/lsp/mason-lspconfig.lua
@@ -1,3 +1,3 @@
 require("mason-lspconfig").setup({
-	ensure_installed = { "sumneko_lua", "clangd", "pylsp", "bashls", "cmake"}
+	ensure_installed = { "sumneko_lua", "clangd", "pylsp", "bashls", "neocmake"}
 })
