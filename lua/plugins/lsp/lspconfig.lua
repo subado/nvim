01e7df35ba5387a4fc5372b8d1ac66a146c5d829
Subado <redcatgamer1980@gmail.com>
2023-01-07 12:16:49 +0400

    Disaple LSP snippets

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -30,6 +30,7 @@ end
 
 -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = false
 
 local lsp_servers = require("mason-lspconfig").get_installed_servers()
 local default = {
