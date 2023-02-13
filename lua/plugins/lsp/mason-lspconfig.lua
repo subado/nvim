460401315fea8a6c908e9df9425a09225f6e5b96
Subado <redcatgamer1980@gmail.com>
2023-02-13 09:10:41 +0400

    Add arduino ls to ensure install

--- lua/plugins/lsp/mason-lspconfig.lua
+++ lua/plugins/lsp/mason-lspconfig.lua
@@ -1,3 +1,3 @@
 require("mason-lspconfig").setup({
-	ensure_installed = { "lua_ls", "clangd", "pylsp", "bashls", "neocmake"}
+	ensure_installed = { "lua_ls", "clangd", "pylsp", "bashls", "neocmake", "arduino_language_server"}
 })
