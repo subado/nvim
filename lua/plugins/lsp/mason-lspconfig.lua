c60108020aa13f8a2d0c8b31f99bfb194ec5ae0f
Subado <redcatgamer1980@gmail.com>
2023-02-15 21:03:12 +0400

    Add html ls

--- lua/plugins/lsp/mason-lspconfig.lua
+++ lua/plugins/lsp/mason-lspconfig.lua
@@ -1,3 +1,3 @@
 require("mason-lspconfig").setup({
-	ensure_installed = { "lua_ls", "clangd", "pylsp", "bashls", "neocmake", "arduino_language_server"}
+	ensure_installed = { "lua_ls", "clangd", "pylsp", "bashls", "neocmake", "arduino_language_server", "html" }
 })
