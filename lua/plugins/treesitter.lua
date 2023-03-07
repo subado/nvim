d078b665f243ed70fc1b08510632667cd4f7b510
subado <redcatgamer1980@gmail.com>
2023-03-07 22:40:58 +0400

    tsx being parse like tsx and like js

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,3 +8,6 @@ require 'nvim-treesitter.configs'.setup {
 		enable = true,
 	},
 }
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
