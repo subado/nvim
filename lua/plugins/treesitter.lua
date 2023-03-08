a8e4d6eb105469e131486d17535ac7fce6a3e661
subado <redcatgamer1980@gmail.com>
2023-03-08 11:04:56 +0400

    Disable ts indent for cpp and c

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,8 +6,11 @@ require 'nvim-treesitter.configs'.setup {
 	},
 	indent = {
 		enable = true,
+		disable = { "cpp", "c" },
 	},
 }
+
+
 
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
