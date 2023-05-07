9a7a95608f7114ad3162fca875cee5890ec09a5b
subado <subado.main@gmail.com>
2023-05-07 13:22:49 +0400

    Add all lsp server to ensure_installed

--- lua/plugins/lsp/mason-lspconfig.lua
+++ lua/plugins/lsp/mason-lspconfig.lua
@@ -1,3 +1,15 @@
 require('mason-lspconfig').setup {
-  ensure_installed = { 'lua_ls', 'clangd', 'pylsp', 'bashls', 'html', 'tsserver', 'cssls', 'tailwindcss' },
+  ensure_installed = {
+    'lua_ls',
+    'clangd',
+    'pylsp',
+    'bashls',
+    'html',
+    'tsserver',
+    'cssls',
+    'tailwindcss',
+    'kotlin_language_server',
+    'neocmake',
+    'svelte',
+  },
 }
