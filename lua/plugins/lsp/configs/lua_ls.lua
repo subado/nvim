7743efb777fd37067306628a39bcf4e1ed23c125
subado <subado.main@gmail.com>
2023-05-19 18:12:31 +0400

    Disable annoying luaasert

--- lua/plugins/lsp/configs/lua_ls.lua
+++ lua/plugins/lsp/configs/lua_ls.lua
@@ -12,6 +12,8 @@ local M = {
       workspace = {
         -- Make the server aware of Neovim runtime files
         library = vim.api.nvim_get_runtime_file('', true),
+        -- Disable annoying luaasert
+        checkThirdParty = false,
       },
       -- Do not send telemetry data containing a randomized but unique identifier
       telemetry = {
