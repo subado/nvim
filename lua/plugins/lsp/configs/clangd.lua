b9d2d7605c6687b1c16e793ddf4b5e8a4abe6c65
subado <subado.main@gmail.com>
2023-11-05 17:29:57 +0400

    Fix clangd_extensions

--- lua/plugins/lsp/configs/clangd.lua
+++ lua/plugins/lsp/configs/clangd.lua
@@ -1,2 +1,8 @@
-local M = require('plugins.clangd_extensions')
+local M = {
+  cmd = { 'clangd', '--completion-style=detailed', '--offset-encoding=utf-16' },
+  on_attach = function(client, bufnr)
+    require("clangd_extensions.inlay_hints").setup_autocmd()
+    require("clangd_extensions.inlay_hints").set_inlay_hints()
+  end
+}
 return M
