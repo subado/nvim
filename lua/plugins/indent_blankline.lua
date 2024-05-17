818b5b5fd5fd2d5f5e102a8afd23f69c31b5d529
subado <subado.main@gmail.com>
2024-05-17 19:31:42 +0400

    Fix index_blankline

--- lua/plugins/indent_blankline.lua
+++ lua/plugins/indent_blankline.lua
@@ -2,4 +2,7 @@ vim.opt.list = true
 -- vim.opt.listchars:append "space:⋅"
 vim.opt.listchars:append('eol:↴')
 
-require('ibl').setup {}
+require('indent_blankline').setup {
+  show_current_context = true,
+  show_current_context_start = true,
+}
